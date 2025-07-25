64ed69319e34b69564a0eff838d58d5e0121860d
Pauzan Rizky Alamsyah <150655832+JansMoriarty@users.noreply.github.com>
2025-07-25 15:16:30 +0700

    add animation to streak and sound

--- lib/pages/habit_list_page.dart
+++ lib/pages/habit_list_page.dart
@@ -17,81 +17,6 @@ class HabitListPage extends StatefulWidget {
 }
 
 class _HabitListPageState extends State<HabitListPage> {
-  void _showStreakOverlay(BuildContext context) {
-    showDialog(
-      context: context,
-      barrierDismissible: true,
-      builder: (_) {
-        return Center(
-          child: Material(
-            color: Colors.transparent,
-            child: Container(
-              width: 280,
-              padding: const EdgeInsets.only(
-                  left: 20, right: 20, top: 0, bottom: 42),
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 0, 123, 199),
-                borderRadius: BorderRadius.circular(38),
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Lottie.asset(
-                    'assets/animations/medal.json',
-                    width: 150,
-                    height: 150,
-                    repeat: true,
-                  ),
-                  const SizedBox(height: 24),
-                  Text(
-                    '1 Day Streak!',
-                    style: GoogleFonts.poppins(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    'Keep going your\ngreat job!',
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.poppins(
-                      color: const Color.fromARGB(211, 255, 255, 255),
-                      fontSize: 14,
-                    ),
-                  ),
-                  const SizedBox(height: 28),
-                  Text(
-                    '24 Days more to Silver!',
-                    style: GoogleFonts.poppins(
-                      color: Colors.amberAccent,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  const SizedBox(height: 12),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: LinearProgressIndicator(
-                        value: 0.3,
-                        minHeight: 10,
-                        backgroundColor: Colors.white24,
-                        valueColor:
-                            AlwaysStoppedAnimation<Color>(Colors.amberAccent),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        );
-      },
-    );
-  }
-
   late DatabaseHelper dbHelper;
 
   int _completedTasks = 0;
@@ -242,6 +167,104 @@ class _HabitListPageState extends State<HabitListPage> {
   // ✅ Letakkan di sini
   bool _allHabitsCompleted(List<Habit> habits) {
     return habits.every((habit) => habit.progress >= habit.quantity);
+  }
+
+  void _showStreakOverlay(BuildContext context) {
+    final player = AudioPlayer();
+    player.play(AssetSource('sounds/streak.wav'));
+    showGeneralDialog(
+      context: context,
+      barrierDismissible: true,
+      barrierLabel: 'Streak Overlay',
+      transitionDuration: const Duration(milliseconds: 600),
+      pageBuilder: (_, __, ___) => const SizedBox.shrink(),
+      transitionBuilder: (context, animation, secondaryAnimation, child) {
+        final curvedAnimation =
+            CurvedAnimation(parent: animation, curve: Curves.elasticOut);
+
+        return ScaleTransition(
+          scale: Tween<double>(begin: 0.8, end: 1.0).animate(curvedAnimation),
+          child: FadeTransition(
+            opacity: animation,
+            child: Center(
+              child: Material(
+                color: Colors.transparent,
+                child: Container(
+                  width: 280,
+                  padding: const EdgeInsets.only(
+                      left: 20, right: 20, top: 0, bottom: 42),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 0, 123, 199),
+                    borderRadius: BorderRadius.circular(38),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Lottie.asset(
+                        'assets/animations/medal.json',
+                        width: 150,
+                        height: 150,
+                        repeat: true,
+                      ),
+                      const SizedBox(height: 24),
+                      Text(
+                        '1 Day Streak!',
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        'Keep going your\ngreat job!',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.poppins(
+                          color: const Color.fromARGB(211, 255, 255, 255),
+                          fontSize: 14,
+                        ),
+                      ),
+                      const SizedBox(height: 28),
+                      Text(
+                        '24 Days more to Silver!',
+                        style: GoogleFonts.poppins(
+                          color: Colors.amberAccent,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      const SizedBox(height: 12),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: LinearProgressIndicator(
+                            value: 0.3,
+                            minHeight: 10,
+                            backgroundColor: Colors.white24,
+                            valueColor: AlwaysStoppedAnimation<Color>(
+                                Colors.amberAccent),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+
+// ⏳ Auto close setelah 4 detik
+    Future.delayed(const Duration(seconds: 4), () {
+      if (Navigator.canPop(context)) {
+        Navigator.of(context, rootNavigator: true).pop();
+
+        
+      }
+    });
   }
 
   // tampilkan overlay confetti
